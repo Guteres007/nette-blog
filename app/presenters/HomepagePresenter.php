4a5a48d92048a3d4b07d1139b7fa62a094597ffa
MartinAndráši <andrasi.m007@gmail.com>
2018-07-27 15:05:11 +0200

    full nette kurz

---
+++ app/presenters/HomepagePresenter.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Presenters;
+use Nette;
+use App\Model\ArticleManager;
+
+
+class HomepagePresenter extends Nette\Application\UI\Presenter
+{
+  private $articleManager;
+
+  public function __construct(ArticleManager $articleManager )
+  {
+    $this->articleManager = $articleManager;
+  }
+
+
+public function renderDefault()
+{
+    $this->template->posts = $this->articleManager->getPublicArticles()->limit(5);
+}
+
+
+}
